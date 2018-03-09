9245bba9f1df6a425d103cd799476077115cc38c
Nardberjean <Nardberjean@users.noreply.github.com>
2018-03-09 14:12:35 -0500

    Update demo_standard.bash

--- demo_standard.bash
+++ demo_standard.bash
@@ -10,6 +10,32 @@ if [[ -v $DRUPAL_PROJECT_DIR ]]; then cd $DRUPAL_PROJECT_DIR; else cd drupal-pro
 
 site_install () { drupal site:install $PROFILE --yes --no-interaction --verbose --langcode=$LANGCODE --db-type=$DB_TYPE --db-host=$DB_HOST --db-port=$DB_PORT --db-user=$USER; }
 
+exitfn () {
+  trap SIGINT              # Restore signal handling for SIGINT
+  drupal database:drop --no-interaction || true
+  if ls web/sites/default/files/.ht.sqlite; then
+    df --print-type web/sites/default/files/.ht.sqlite
+    pushd web/sites/default/files; du -sch .ht.sqlite; popd
+    # rm web/sites/default/files/.ht.sqlite; # Does not work because used by server
+    cat <<- EOM | sqlite3 web/sites/default/files/.ht.sqlite
+      PRAGMA writable_schema = 1;
+      delete from sqlite_master where type in ('table', 'index', 'trigger');
+      PRAGMA writable_schema = 0;
+      VACUUM;
+      PRAGMA INTEGRITY_CHECK;
+EOM
+    # https://stackoverflow.com/questions/525512/drop-all-tables-command
+    $db_target=$(readlink --canonicalize web/sites/default/files/.ht.sqlite)
+    rm $db_target
+    rmdir $(dirname $db_target)
+  fi
+  if ls web/sites/default/settings.php; then
+    chmod u+w web/sites/default
+    rm -f web/sites/default/settings.php
+  fi
+  exit
+}
+
 case $DB_TYPE in
   "mysql")
     echo MySQL;
@@ -50,28 +76,8 @@ echo $PROFILE $LANGCODE $DB_TYPE
 time site_install
 drush core:status
 drush core:requirements
-drupal server --learning # --yes --no-interaction
-# printf 'HEAD / HTTP/1.1\r\n\r\n' | socat - TCP4:localhost:8088,forever # Waiting for server to connect.
-# elinks http://localhost:8088/ -dump-color-mode 4 -dump
 
-drupal database:drop --no-interaction || true
-if ls web/sites/default/files/.ht.sqlite; then
-  df --print-type web/sites/default/files/.ht.sqlite
-  pushd web/sites/default/files; du -sch .ht.sqlite; popd
-  # rm web/sites/default/files/.ht.sqlite; # Does not work because used by server
-  cat <<- EOM | sqlite3 web/sites/default/files/.ht.sqlite
-    PRAGMA writable_schema = 1;
-    delete from sqlite_master where type in ('table', 'index', 'trigger');
-    PRAGMA writable_schema = 0;
-    VACUUM;
-    PRAGMA INTEGRITY_CHECK;
-EOM
-  # https://stackoverflow.com/questions/525512/drop-all-tables-command
-  $db_target=$(readlink --canonicalize web/sites/default/files/.ht.sqlite)
-  rm $db_target
-  rmdir $(dirname $db_target)
-fi
-if ls web/sites/default/settings.php; then
-  chmod u+w web/sites/default
-  rm -f web/sites/default/settings.php
-fi
+trap "exitfn" INT
+drupal server --learning # --yes --no-interaction
+trap SIGINT
+
