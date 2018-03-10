1dd3d8197eef440dab2fbb2f2c9244d00ec3417e
Nardberjean <Nardberjean@users.noreply.github.com>
2018-03-10 07:42:39 -0500

    Update demo_standard.bash

--- demo_standard.bash
+++ demo_standard.bash
@@ -6,6 +6,38 @@ export PROFILE=standard
 # export LANGCODE=... could be extracted from $LANG
 export COMPOSER_NO_INTERACTION=1
 
+# Initialize all the option variables.
+# This ensures we are not contaminated by variables from the environment.
+# http://mywiki.wooledge.org/BashFAQ/035
+server_bypass=0
+verbose=0
+
+while :; do
+    case $1 in
+        -h|-\?|--help)
+            show_help    # Display a usage synopsis.
+            exit
+            ;;
+        -b|--server-bypass)
+            server_bypass=1
+            ;;
+        -v|--verbose)
+            verbose=$((verbose + 1))  # Each -v adds 1 to verbosity.
+            ;;
+        --)              # End of all options.
+            shift
+            break
+            ;;
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            ;;
+        *)               # Default case: No more options, so break out of the loop.
+            break
+    esac
+
+    shift
+done
+
 bash composer_create_drupal-project.bash
 if [[ -v $DRUPAL_PROJECT_DIR ]]; then cd $DRUPAL_PROJECT_DIR; else cd drupal-project; fi;
 
@@ -79,6 +111,11 @@ echo $PROFILE $LANGCODE $DB_TYPE
 time site_install
 drush core:status
 drush core:requirements
+
+if server_bypass; then
+  echo Exit bypassing demo webserver
+  exitfn
+fi
 
 trap "exitfn" INT
 drupal server --learning # --yes --no-interaction
