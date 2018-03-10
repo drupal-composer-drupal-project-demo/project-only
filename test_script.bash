5dc77fad55728960228965aa284fac7fb7860abe
Nardberjean <Nardberjean@users.noreply.github.com>
2018-03-10 11:39:51 -0500

    Update test_script.bash

--- test_script.bash
+++ test_script.bash
@@ -18,7 +18,7 @@ case $DB_TYPE in
     ;;
   "sqlite")
     echo SQLite;
-    if [[ ! -v DB_URL ]]; then export DB_URL=$DB_TYPE://; fi
+    if [[ ! -v DB_URL ]]; then export DB_URL=$DB_TYPE://web/sites/default/files/.ht.sqlite; fi
     echo DB_URL=$DB_URL
     ;;
   "pgsql")
@@ -43,38 +43,39 @@ case $DB_TYPE in
     exit;
     ;;
 esac
+for site_install in drush_site_install console_site_install; do
+  for profile in minimal standard; do
+    for langcode in en fr; do
+      export PROFILE=$profile
+      export LANGCODE=$langcode
+      echo $PROFILE $LANGCODE $DB_TYPE
 
-for profile in minimal standard; do
-  for langcode in en fr; do
-    export PROFILE=$profile
-    export LANGCODE=$langcode
-    echo $PROFILE $LANGCODE $DB_TYPE
+      time $site_install
+      drush core:status
+      drush core:requirements
+      drupal server --yes --no-interaction --learning & printf 'HEAD / HTTP/1.1\r\n\r\n' | socat - TCP4:localhost:8088,forever # Waiting for server to connect.
+      elinks http://localhost:8088/ -dump-color-mode 4 -dump
+      # - elinks http://localhost:8088/core/install.php?langcode=en -dump-color-mode 4 -dump
 
-    time console_site_install
-    drush core:status
-    drush core:requirements
-    drupal server --yes --no-interaction --learning & printf 'HEAD / HTTP/1.1\r\n\r\n' | socat - TCP4:localhost:8088,forever # Waiting for server to connect.
-    elinks http://localhost:8088/ -dump-color-mode 4 -dump
-    # - elinks http://localhost:8088/core/install.php?langcode=en -dump-color-mode 4 -dump
-
-    drupal database:drop --no-interaction || true
-    if ls web/sites/default/files/.ht.sqlite; then
-      readlink --canonicalize web/sites/default/files/.ht.sqlite
-      df --print-type web/sites/default/files/.ht.sqlite
-      pushd web/sites/default/files; du -sch .ht.sqlite; popd
-      # rm web/sites/default/files/.ht.sqlite; # Does not work because used by server
-      cat <<- EOM | sqlite3 web/sites/default/files/.ht.sqlite
-        PRAGMA writable_schema = 1;
-        delete from sqlite_master where type in ('table', 'index', 'trigger');
-        PRAGMA writable_schema = 0;
-        VACUUM;
-        PRAGMA INTEGRITY_CHECK;
+      drupal database:drop --no-interaction || true
+      if ls web/sites/default/files/.ht.sqlite; then
+        readlink --canonicalize web/sites/default/files/.ht.sqlite
+        df --print-type web/sites/default/files/.ht.sqlite
+        pushd web/sites/default/files; du -sch .ht.sqlite; popd
+        # rm web/sites/default/files/.ht.sqlite; # Does not work because used by server
+        cat <<- EOM | sqlite3 web/sites/default/files/.ht.sqlite
+          PRAGMA writable_schema = 1;
+          delete from sqlite_master where type in ('table', 'index', 'trigger');
+          PRAGMA writable_schema = 0;
+          VACUUM;
+          PRAGMA INTEGRITY_CHECK;
 EOM
-      # https://stackoverflow.com/questions/525512/drop-all-tables-command
-    fi
-    if ls web/sites/default/settings.php; then
-      chmod u+w web/sites/default
-      rm -f web/sites/default/settings.php
-    fi
-  done;
+        # https://stackoverflow.com/questions/525512/drop-all-tables-command
+      fi
+      if ls web/sites/default/settings.php; then
+        chmod u+w web/sites/default
+        rm -f web/sites/default/settings.php
+      fi
+    done
+  done
 done
