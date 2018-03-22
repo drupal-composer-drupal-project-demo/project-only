ce22bb5fada32cda6c95bc73bb9461a8ff7df938
Nardberjean <Nardberjean@users.noreply.github.com>
2018-03-22 10:33:51 -0400

    Update test_script.bash

--- test_script.bash
+++ test_script.bash
@@ -19,7 +19,7 @@ for site_install in ${site_install_commands[*]}; do
       drush core:requirements
       ls --color
       find . -name core
-      phpdbg -qrr vendor/bin/phpunit --testsuite unit # -c core
+      phpdbg -qrr vendor/bin/phpunit --configuration vendor/drupal/core --testsuite unit
       drupal server --yes --no-interaction --learning & printf 'HEAD / HTTP/1.1\r\n\r\n' | socat - TCP4:localhost:8088,forever # Waiting for server to connect.
       elinks http://localhost:8088/ -dump-color-mode 4 -dump
       # - elinks http://localhost:8088/core/install.php?langcode=en -dump-color-mode 4 -dump
